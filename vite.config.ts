728c7a543eee38d9b1f688461236871cfab912dd
alan lenisa <alan.lenisa@gmail.com>
2026-03-19 16:35:04 +0100

    260319 h1634

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // './' keeps asset paths relative — required for GitHub Pages subdirectory hosting
-  base: './',
+  //base: './',
+  base:'/rifle-simulator-ts/',
   build: {
     outDir: 'dist',
     target: 'es2020',
